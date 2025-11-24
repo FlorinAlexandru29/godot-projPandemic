90a0c3d97b5e68fc3719028bfd38f0f0809b0e28
Shappy99 <142829878+Shappy99@users.noreply.github.com>
2025-11-24 14:33:49 +0200

    timerAdded

--- Scripts/timer.gd
+++ Scripts/timer.gd
@@ -13,17 +13,17 @@ func _add_funds(bonusFunds):
 	
 func _on_timeout():
 	if day<=30:
-		$timeLabel.text=str("Month ", month, ", Day ",day)
-		$fundsLabel.text=str("Funds: ", funds)
+		$VBoxContainer/timeLabel.text=str("Month ", month, ", Day ",day)
+		$VBoxContainer/fundsLabel.text=str("Funds: ", funds)
 		day+=1
 	elif month<12:
 		month+=1
 		_add_funds(10000)
-		$fundsLabel.text=str("Funds: ", funds)
+		$VBoxContainer/fundsLabel.text=str("Funds: ", funds)
 		if month==12:
-			$timeLabel.text="Game over"
+			$VBoxContainer/timeLabel.text="Game over"
 		else:
 			day=1
-			$timeLabel.text=str("Month ", month, ", Day ",day)
+			$VBoxContainer/timeLabel.text=str("Month ", month, ", Day ",day)
 	else:
 		pass
